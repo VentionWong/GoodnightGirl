96f9f7a3d0347495d7091ba31d3e2b56988eb838
ventionwang <214198501@qq.com>
2017-08-13 22:16:23 +0800

    更新播放逻辑，之前会有多个播放出现

--- js/download.audio.js
+++ js/download.audio.js
@@ -1,49 +1,32 @@
 ﻿// For: girlinfo.html
 
-/*<img>设置图片
-     *1.从本地获取,如果本地存在,则直接设置图片
-     *2.如果本地不存在则联网下载,缓存到本地,再设置图片
+/*<img>设置音频
+     *1.从本地获取,如果本地存在,则直接设置音频
+     *2.如果本地不存在则联网下载,缓存到本地,再设置音频
      * */
     function setImg(imgId, loadUrl) {
             if (imgId == null || loadUrl == null) return;
-            //图片下载成功 默认保存在本地相对路径的"_downloads"文件夹里面, 如"_downloads/logo.jpg"
+            //音频下载成功 默认保存在本地相对路径的"_downloads"文件夹里面, 如"_downloads/logo.jpg"
             var filename = loadUrl.substring(loadUrl.lastIndexOf("/") + 1, loadUrl.length);
             var relativePath = "_downloads/" + filename;
-            //检查图片是否已存在
+            //检查音频是否已存在
             plus.io.resolveLocalFileSystemURL(relativePath, function(entry) {
-                console.log("图片存在,直接设置=" + relativePath);
-                //如果文件存在,则直接设置本地图片
-                setImgFromLocal(imgId, relativePath);
+                console.log("音频存在,直接设置=" + relativePath);
+                
             }, function(e) {
-                console.log("图片不存在,联网下载=" + relativePath);
-                //如果文件不存在,联网下载图片
+                console.log("音频不存在,联网下载=" + relativePath);
+                //如果文件不存在,联网下载音频
                 setImgFromNet (imgId,loadUrl,relativePath);
             });
             //触发submit按钮的点击事件
 			mui.trigger(myplayer,'tap');
         }
 
-    /*给图片标签<img>设置本地图片
-     * imgId 图片标签<img>的id
-     * relativePath 本地相对路径 例如:"_downloads/logo.jpg"
-     */
-    function setImgFromLocal(imgId, relativePath) {
-            //本地相对路径("_downloads/logo.jpg")转成SD卡绝对路径("/storage/emulated/0/Android/data/io.dcloud.HBuilder/.HBuilder/downloads/logo.jpg");
-            var sd_path = plus.io.convertLocalFileSystemURL(relativePath);
-            
-            if(plus.os.name=="iOS"){
-		        sd_path = "startPlay('"+relativePath+"');";
-		    }else{
-		    	sd_path = "startPlay('"+sd_path+"');";
-		    }
-            //给<img>设置图片
-            sd_path = "startPlay('"+relativePath+"');";
-            $id(imgId).setAttribute("onclick", sd_path);
-        }
+    
 
-    /*联网下载图片,并设置给<img>*/
+    /*联网下载音频,并设置给<img>*/
     function setImgFromNet (imgId,loadUrl,relativePath) {
-        //先设置下载中的默认图片
+        //先设置下载中的默认音频
         //$id(imgId).setAttribute("src", "../../images/logo.png");
         mui.toast("下载音频中...");
         //创建下载任务
@@ -51,9 +34,8 @@
             if (status == 200) {
                 //下载成功
                 console.log("下载成功=" + relativePath);
-                setImgFromLocal(imgId, d.filename);
             } else {
-                //下载失败,需删除本地临时文件,否则下次进来时会检查到图片已存在
+                //下载失败,需删除本地临时文件,否则下次进来时会检查到音频已存在
                 console.log("下载失败=" + status+"=="+relativePath);
                 //dtask.abort();//文档描述:取消下载,删除临时文件;(但经测试临时文件没有删除,故使用delFile()方法删除);
                 if (relativePath!=null)
